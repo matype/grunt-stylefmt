1775f3ab215b52488a0cf1a1d0cd3b8f5ecbffac
morishitter <morishitter@gmail.com>
2015-08-05 10:33:58 +0900

    implement

--- tasks/cssfmt.js
+++ tasks/cssfmt.js
@@ -1,31 +1,15 @@
-/*
- * grunt-cssfmt
- * https://github.com/morishitter
- *
- * Copyright (c) 2015 Masaaki Morishita
- * Licensed under the MIT license.
- */
-
-'use strict';
-
 module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var cssfmt = require('cssfmt');
 
   grunt.registerMultiTask('cssfmt', 'Grunt plugin for CSSfmt', function () {
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+    var options = this.options ({
+      separator: '\n'
     });
 
-    // Iterate over all specified file groups.
     this.files.forEach(function (file) {
-      // Concat specified files.
       var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
         if (!grunt.file.exists(filepath)) {
           grunt.log.warn('Source file "' + filepath + '" not found.');
           return false;
@@ -33,17 +17,12 @@ module.exports = function (grunt) {
           return true;
         }
       }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
+        var cssFile = grunt.file.read(filepath);
+        return cssfmt(cssFile).toString();
       }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
       grunt.file.write(file.dest, src);
 
-      // Print a success message.
       grunt.log.writeln('File "' + file.dest + '" created.');
     });
   });
